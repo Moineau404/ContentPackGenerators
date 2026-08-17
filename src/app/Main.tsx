00ec87e76bbe26d1dc089683f915466bfbc91caf
Moineau <maitrecorbeau2412@gmail.com>
2026-08-17 14:16:59 +0200

    Update Main.tsx

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -3,9 +3,6 @@ import '../styles/global.css'
 import '../styles/main.css'
 import '../styles/nodes.css'
 import { App } from './App.js'
-import { LocaleProvider, ProjectProvider, StoreProvider, ThemeProvider, TitleProvider, VersionProvider } from './contexts/index.js'
-import { ModalProvider } from './contexts/Modal.jsx'
-import { SpyglassProvider } from './contexts/Spyglass.jsx'
 
 function Main() {
 	return (
