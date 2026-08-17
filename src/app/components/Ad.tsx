956723bb1eee089bfa2b5ad4fccd331a6ec0df5f
Moineau <maitrecorbeau2412@gmail.com>
2026-08-17 15:18:36 +0200

    Update Ad.tsx

--- src/app/components/Ad.tsx
+++ src/app/components/Ad.tsx
@@ -1,11 +1,9 @@
-import { useEffect } from 'preact/hooks'
-
 declare const ethicalads: any
 
 type AdProps = {
 	type: 'text' | 'image',
 	id: string,
 }
-export function Ad({ type, id }: AdProps) {
+export function Ad({}: AdProps) {
 	return <></>
 }
